ad9d94f2312a26a1f45cbb5b8da64584725ee0f7
shhung <shhung@csie.io>
2024-01-04 18:49:06 +0800

    Enable print out the stage for debugging PipelineTest is for CPU module

--- src/main/scala/riscv/core/CPU.scala
+++ src/main/scala/riscv/core/CPU.scala
@@ -25,17 +25,34 @@ class CPU extends Module {
     val wb_reg_write_source = UInt(2.W)
     val reg_write_enable    = Output(Bool())
     val reg_write_address   = Output(UInt(Parameters.PhysicalRegisterAddrWidth))
+    override def toPrintable: Printable = {
+      cf"  mem_RE: $memory_read_enable" +
+      cf"  mem_WE: $memory_write_enable" +
+      cf"  wb_reg_src: $wb_reg_write_source" +
+      cf"  reg_WE: $reg_write_enable" +
+      cf"  reg_WA: $reg_write_address\n"
+    }
   }
 
   class FD_EXBundle extends Bundle {
     val instruction         = UInt(Parameters.InstructionWidth)
     val instruction_address = UInt(Parameters.AddrWidth)
     val immediate           = UInt(Parameters.DataWidth)
-    val aluop1_source       = UInt(1.W)
-    val aluop2_source       = UInt(1.W)
+    val ex_aluop1_source    = UInt(1.W)
+    val ex_aluop2_source    = UInt(1.W)
     val reg_read_address1   = UInt(Parameters.PhysicalRegisterAddrWidth)
     val reg_read_address2   = UInt(Parameters.PhysicalRegisterAddrWidth)
     val wbcontrol           = new WBControl
+    override def toPrintable: Printable = {
+      cf"  inst: 0x$instruction%x" +
+      cf"  instAddr: $instruction_address" +
+      cf"  imm: $immediate" +
+      cf"  op1_src: $ex_aluop1_source" +
+      cf"  op2_src: $ex_aluop2_source" +
+      cf"  reg1_RA: $reg_read_address1" +
+      cf"  reg2_RA: $reg_read_address2" +
+      cf"  $wbcontrol"
+    }
   }
 
   class EX_WBBundle extends Bundle {
@@ -44,6 +61,13 @@ class CPU extends Module {
     val mem_alu_result      = UInt(Parameters.DataWidth)
     val reg2_data           = UInt(Parameters.DataWidth)
     val wbcontrol           = new WBControl
+    override def toPrintable: Printable = {
+      cf"  inst: 0x$instruction%x" +
+      cf"  instAddr: $instruction_address" +
+      cf"  alu_out: $mem_alu_result" +
+      cf"  reg2_data: $reg2_data" +
+      cf"  $wbcontrol"
+    }
   }
 
   val fd_ex = RegInit(0.U.asTypeOf(new FD_EXBundle))
@@ -65,19 +89,23 @@ class CPU extends Module {
   fd_ex.instruction         := inst_fetch.io.instruction
   fd_ex.instruction_address := inst_fetch.io.instruction_address
   fd_ex.immediate           := id.io.ex_immediate
-  fd_ex.aluop1_source       := id.ex_aluop1_source
-  fd_ex.aluop2_source       := id.ex_aluop2_source
-  fd_ex.read_address1       := id.io.regs_reg1_read_address
-  fd_ex.read_address2       := id.io.regs_reg2_read_address
+  fd_ex.ex_aluop1_source       := id.io.ex_aluop1_source
+  fd_ex.ex_aluop2_source       := id.io.ex_aluop2_source
+  fd_ex.reg_read_address1   := id.io.regs_reg1_read_address
+  fd_ex.reg_read_address2   := id.io.regs_reg2_read_address
   fd_ex.wbcontrol.memory_read_enable  := id.io.memory_read_enable
   fd_ex.wbcontrol.memory_write_enable := id.io.memory_write_enable
-  fd_ex.wbcontrol.regs_write_source   := id.io.regs_write_source
-  fe_ex.wbcontrol.reg_write_enable    := id.io.reg_write_enable
-  fe_ex.wbcontrol.wb_reg_write_source := id.io.wb_reg_write_source
+  fd_ex.wbcontrol.wb_reg_write_source := id.io.wb_reg_write_source
+  fd_ex.wbcontrol.reg_write_enable    := id.io.reg_write_enable
+  fd_ex.wbcontrol.wb_reg_write_source := id.io.wb_reg_write_source
+
+  // debug
+  printf(cf"fd_ex = $fd_ex")
+  // 
 
   // EXE
-  regs.io.read_address1 := fd_ex.regs_reg1_read_address
-  regs.io.read_address2 := fd_ex.regs_reg2_read_address
+  regs.io.read_address1 := fd_ex.reg_read_address1
+  regs.io.read_address2 := fd_ex.reg_read_address2
   regs.io.debug_read_address := io.debug_read_address
   io.debug_read_data         := regs.io.debug_read_data
 
@@ -101,7 +129,7 @@ class CPU extends Module {
   }.otherwise {
     ex.io.reg2_data := regs.io.read_data1
   }
-  ex.io.immediate           := fd_ex.ex_immediate
+  ex.io.immediate           := fd_ex.immediate
   ex.io.aluop1_source       := fd_ex.ex_aluop1_source
   ex.io.aluop2_source       := fd_ex.ex_aluop2_source
 
@@ -112,9 +140,14 @@ class CPU extends Module {
   ex_wb.reg2_data           := regs.io.read_data2
   ex_wb.wbcontrol           := fd_ex.wbcontrol
 
+  // debug
+  printf(cf"ex_wb = $ex_wb")
+  printf("\n")
+  // 
+
   // WB
   mem.io.alu_result          := ex_wb.mem_alu_result
-  mem.io.reg2_data           := ex_wb.read_data2
+  mem.io.reg2_data           := ex_wb.reg2_data
   mem.io.memory_read_enable  := ex_wb.wbcontrol.memory_read_enable
   mem.io.memory_write_enable := ex_wb.wbcontrol.memory_write_enable
   mem.io.funct3              := ex_wb.instruction(14, 12)
